f5a515aa8e6e80c6196fa8357e798ce4907c134b
Fernando González <rmfernando@outlook.com>
2019-10-24 12:06:22 -0600

    Heroku and some uodates to files...

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ environment = os.getenv("ENVIRONMENT", "development")
 
 app = Flask(__name__)
 
-
 with open('info.json') as f:
   data = json.load(f)
 
-
 @app.route('/') 
+
 def index():
 
     return render_template("index.html")
@@ -24,6 +23,7 @@ if __name__ == '__main__':
     debug=False
     if environment == "development" or environment == "local":
         debug=True
+
     print("Local change")
 
     app.run(host="0.0.0.0", debug=debug)
