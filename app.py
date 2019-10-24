83f329a2883f52b1bc548eb17d02ef3788f2a4bb
Fernando González <rmfernando@outlook.com>
2019-10-24 08:18:05 -0600

    Updtes...

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, jsonify, redirect
 import yaml
 import os
+import json
 
 developer = os.getenv("DEVELOPER", "User")
 environment = os.getenv("ENVIRONMENT", "development")
 
 app = Flask(__name__)
+
+
+with open('info.json') as f:
+  data = json.load(f)
 
 
 @app.route('/') 
@@ -16,4 +21,9 @@ def index():
 
 if __name__ == '__main__':
 
-  app.run(host="0.0.0.0", debug=True)
+    debug=False
+    if environment == "development" or environment == "local":
+        debug=True
+    print("Local change")
+
+    app.run(host="0.0.0.0", debug=debug)
