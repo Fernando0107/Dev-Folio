2c04f81f6959e5cdbd9fadb7bbda7f6a433b64ab
Fernando González <rmfernando@outlook.com>
2019-11-23 18:18:21 -0600

    about me updated, and skills added.

--- app.py
+++ app.py
@@ -12,11 +12,14 @@ with open('info.json') as f:
   data = json.load(f)
 
 
+print(data["about"])
+
+
 @app.route('/') 
 
 def index():
 
-    return render_template("index.html", name=data['info']["name"], phone=data['info']["phone"], mail=data['info']["mail"], profile=data['info']["profile"])
+    return render_template("index.html", name=data['info']["name"], phone=data['info']["phone"], mail=data['info']["mail"], profile=data['info']["profile"], about=data["about"], skill=data["skill"]["s1"], skill2=data["skill"]["s2"], skill3=data["skill"]["s3"], skill4=data["skill"]["s4"])
 
 
 if __name__ == '__main__':
